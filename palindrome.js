366327edb33286cbdfa64d065fa0ff59019976ea
Madhu Chakravarthy <ncmadhu@gmail.com>
2017-04-16 23:17:35 +0530

    Added code to check palindrome of a string

---
+++ palindrome.js
@@ -0,0 +1,32 @@
+// Author: Madhu Chakravarthy
+// Date: 16-04-2-17
+
+
+var palindrome = function(string) {
+
+    if(string) {
+        var i = 0
+        var j = string.length - 1
+        var isPalindrome = true
+        for(; i <= string.length/2; i++,j--) {
+            if(string[i] != string[j]) {
+                isPalindrome = false
+                break
+            }
+            if(i >= j) {
+                break
+            }
+        }
+        return isPalindrome
+    } else {
+        return false
+    }
+}
+
+console.log("EMPTY STRING: " + palindrome(""))
+console.log("M: " + palindrome("M"))
+console.log("ABBA: " + palindrome("ABBA"))
+console.log("ASDA: " +  palindrome("ASDA"))
+console.log("MALAYALAM: " + palindrome("MALAYALAM"))
+console.log("MALXYALAM: " + palindrome("MALXYALAM"))
+
