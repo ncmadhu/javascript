db4b1fc66e9b9a033d1863e6b2cf01ebfbb871a0
Madhu Chakravarthy <madhuc@altencalsoftlabs.com>
2017-04-20 00:19:55 +0530

    After failure a lesson

---
+++ fibonacci2.js
@@ -0,0 +1,35 @@
+// Author: Madhu Chakravarthy
+// Date: 20-04-2017
+
+var fibonacci_series = function(n) {
+    
+    var arr = []
+    var fibonacci_series2 = function(n) {
+        if(n===0) {
+            arr.push(0)
+        } else if(n===1) {
+            arr.push(0,1)
+        } else {
+            fibonacci_series2(n-1,arr)
+            arr.push(arr[arr.length -1] + arr[arr.length-2])
+        }
+    }
+    fibonacci_series2(n)
+    return arr
+}
+console.log(fibonacci_series(0))
+console.log(fibonacci_series(1))
+console.log(fibonacci_series(2))
+console.log(fibonacci_series(3))
+console.log(fibonacci_series(4))
+console.log(fibonacci_series(5))
+console.log(fibonacci_series(6))
+console.log(fibonacci_series(7))
+console.log(fibonacci_series(8))
+console.log(fibonacci_series(9))
+console.log(fibonacci_series(10))
+console.log(fibonacci_series(11))
+console.log(fibonacci_series(12))
+console.log(fibonacci_series(13))
+console.log(fibonacci_series(14))
+  
